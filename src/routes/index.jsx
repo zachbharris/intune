f36773b5de54f3847a37bb0b42cda83c99a7da8f
zachbharris <zachbharris94@gmail.com>
2018-10-13 12:02:43 -0500

    working transition group bruhhh

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import HomePage from '../pages/Home';
 import TuningPage from '../pages/Tuning';
 import NotFoundPage from '../pages/NotFound';
 
 const Routes = () => (
-  <div className="app">
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/tuning/:tuning" component={TuningPage} />
-      <Route component={NotFoundPage} />
-    </Switch>
+  <div>
+    <Route render={({ location }) => (
+      <TransitionGroup className="app">
+        <CSSTransition
+          key={location.key}
+          timeout={30000}
+          classNames="fade"
+        >
+          <Switch location={location}>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/tuning/:tuning" component={TuningPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    )} />
   </div>
 );
 
